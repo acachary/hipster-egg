6b1bc72e9bbbcbb1603e08ef4116ff630dd0e52f
acharya <acharya@cisco.com>
2019-02-25 12:21:20 -0800

    Adding vuetify. Created homepage.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container
+    fluid>
+    <v-layout
+      align-center
+      justify-center
+      column
+      fill-height
+      id="home_page_container"
+    >
+      <img
+        src="@/assets/easter_hipster.svg"
+        alt=""
+        id="hipster_egg_img"
+        class="home-img"
+      >
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
+
+<style lang="scss">
+.home-img{
+  width: 70%;
+}
+</style>
